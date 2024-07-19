06cf675fa298746e0618fe8040b7a49823f6f39a
chlaslwnd <choimin0915@gmail.com>
2024-07-19 17:22:32 +0900

    240719 spring.in예제와 bootstrap을 이용한 복합적 코드

--- src/main/java/com/boot/board_240718/controller/BoardController.java
+++ src/main/java/com/boot/board_240718/controller/BoardController.java
@@ -2,16 +2,20 @@ package com.boot.board_240718.controller;
 
 import com.boot.board_240718.model.Board;
 import com.boot.board_240718.repository.BoardRepository;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/board")
+@Slf4j
 public class BoardController {
     @Autowired
     private BoardRepository boardRepository;
@@ -22,5 +26,41 @@ public class BoardController {
         model.addAttribute("boards",boards);
 
         return "board/list";
+    }
+
+    @GetMapping("/form")
+//    public String form(Model model){
+//    public String form(Model model, @RequestParam Long id){
+    public String form(Model model, @RequestParam(required = false) Long id){
+        log.info("@# GetMapping form()");
+        log.info("@# id=>"+id);
+
+//        id가 not null 일 경우 model에 board를 담는다
+        if (id != null){
+//            둘중 하나 가능
+//            Optional<Board> board = boardRepository.findById(id);
+            Board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board",board);
+            log.info("@# board=>"+board);
+        } else {
+//            id 가 null 일 경우
+            model.addAttribute("board", new Board());
+        }
+
+        return "board/form";
+    }
+
+    @PostMapping("/form")
+//    public String form(@ModelAttribute Board board, Model model) {
+    public String checkPersonInfo(@Valid Board board, BindingResult bindingResult) {
+//        오류가 날시
+        if (bindingResult.hasErrors()) {
+            return "board/form";
+        }
+
+//        insert 쿼리 없이 Repository로
+        boardRepository.save(board);
+
+        return "redirect:/board/list";
     }
 }
--- src/main/java/com/boot/board_240718/model/Board.java
+++ src/main/java/com/boot/board_240718/model/Board.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Entity //JPA가 관리하는 클래스
@@ -12,8 +14,8 @@ public class Board {
     @Id //pk
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @NotNull //필수 값
+    @Size(min=2, max=30) //제목 최소 2자이상 최대 30자
     private String title;
     private String content;
 }
-
-
